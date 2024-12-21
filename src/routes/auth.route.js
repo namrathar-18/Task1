27500f536fbcab070ea031cd7b49cfcdf366b2ff
namrathar-18 <namrp.18@gmail.com>
2024-12-21 20:39:24 +0530

    Delete src directory

--- src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import  { User } from "../models/user.model.js"; 
-import { authCallback } from "../../controller/auth.controller.js";
-
-const router = Router();
-
-router.post("/callback", authCallback);
-
-export default router
