183c96b572cd7c1e165022c6e14ee6dbe5dac28a
namrathar-18 <namrp.18@gmail.com>
2024-12-21 11:30:02 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import { clerkMiddleware } from '@clerk/express';
+import { connectDB } from "./lib/db.js";
+
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statRoutes from "./routes/stat.route.js";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse req.body
+
+app.use(clerkMiddleware()); // add auth to req obj => req.auth.userId
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port "+ PORT);
+    connectDB();
+});
